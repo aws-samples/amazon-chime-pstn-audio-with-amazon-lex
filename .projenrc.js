78e05e2edccce39a8c4fdf5941d4807085610d11
Court Schuett <schuettc@amazon.com>
2022-05-26 13:58:04 -0500

    fix: update deps

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,6 @@
 const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.20.0',
+  cdkVersion: '2.25.0',
   license: 'MIT-0',
   author: 'Court Schuett',
   copyrightOwner: 'Amazon.com, Inc.',
